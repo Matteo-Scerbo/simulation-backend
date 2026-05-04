2a2758d2a73a700932a14dda4f9d38082ffbb710
Marco Berzborn <m.berzborn@tue.nl>
2026-05-04 21:44:28 +0200

    dg_method: Split tests - Move fixture definitions to conftest - Move fixture tests to test_fixtures.py

---
+++ dg_method/tests/conftest.py
@@ -0,0 +1,54 @@
+import json
+import os
+import pytest
+import shutil
+import tempfile
+from pathlib import Path
+
+
+def default_data_path():
+    """Get the path to the default data folder."""
+    return os.path.join(
+        os.path.dirname(os.path.abspath(__file__)))
+
+
+def load_default_input_data():
+    """Load the example input data."""
+    with open(os.path.join(
+            default_data_path(),
+            "test_input_edg_acoustics.json"), 'r') as f:
+        data = json.load(f)
+
+    return data
+
+
+@pytest.fixture
+def default_input_data():
+    """Fixture to load the example input data."""
+    return load_default_input_data()
+
+
+@pytest.fixture
+def create_temporary_input_file():
+    """Fixture to create a temporary input JSON file which can be reused to
+    write results to."""
+    input_tmp = load_default_input_data()
+    geo_file = os.path.join(
+        default_data_path(), "test_room_edg_acoustics.geo")
+    msh_file = os.path.join(
+        default_data_path(), "test_room_edg_acoustics.msh")
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmp_path = Path(tmpdirname) / "temp_input.json"
+        shutil.copy(geo_file, Path(tmpdirname))
+        shutil.copy(msh_file, Path(tmpdirname))
+        input_tmp['geo_path'] = os.path.join(
+            tmpdirname, "test_room_edg_acoustics.geo")
+        input_tmp['msh_path'] = os.path.join(
+            tmpdirname, "test_room_edg_acoustics.msh")
+        with open(tmp_path, 'w') as f:
+            json.dump(input_tmp, f)
+
+        yield str(tmp_path)
+
+    return str(tmp_path)

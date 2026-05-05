9077dea53ec75b63b1ed1ca1567b4e2919b28d62
Marco Berzborn <m.berzborn@tue.nl>
2026-05-05 14:24:36 +0200

    de_method: Move cli test into separate file and use subprocess.run to specify more explicitly specify the environment in which the cli is executed

--- de_method/tests/test_acousticDE.py
+++ de_method/tests/test_acousticDE.py
@@ -1,11 +1,7 @@
-import os
 import json
 import numpy as np
 
 from de_interface import DEMethod
-
-
-
 
 
 def test_acoustic_de(create_temporary_input_file):
@@ -17,29 +13,6 @@ def test_acoustic_de(create_temporary_input_file):
 
     with open(create_temporary_input_file, 'r') as f:
          data = json.load(f)
-
-    rir = np.array(data['results'][0]['responses'][0]['receiverResults'][0][
-        "data"
-    ])
-
-    assert rir is not None
-    assert len(rir) > 0
-    assert isinstance(rir, np.ndarray)
-    assert np.any(np.abs(rir) >= 1e-6)
-
-
-def test_de_method_cli(create_temporary_input_file):
-    """Test the DE method CLI.
-    """
-    # Simulate running the CLI by setting the environment variable and
-    # calling the main function
-    os.environ["JSON_PATH"] = create_temporary_input_file
-
-    # Run the main function
-    os.system("python -m de_interface")
-
-    with open(create_temporary_input_file, 'r') as f:
-        data = json.load(f)
 
     rir = np.array(data['results'][0]['responses'][0]['receiverResults'][0][
         "data"
---
+++ de_method/tests/test_acousticDE_cli.py
@@ -0,0 +1,28 @@
+import os
+import json
+import numpy as np
+import subprocess
+import sys
+
+
+def test_de_method_cli(create_temporary_input_file):
+    """Test the DE method CLI.
+    """
+    # Simulate running the CLI by setting the environment variable and
+    # calling the main function
+    subprocess.run(
+        [sys.executable, "-m", "de_interface"],
+        check=True,
+        env={**os.environ, "JSON_PATH": create_temporary_input_file})
+
+    with open(create_temporary_input_file, 'r') as f:
+        data = json.load(f)
+
+    rir = np.array(data['results'][0]['responses'][0]['receiverResults'][0][
+        "data"
+    ])
+
+    assert rir is not None
+    assert len(rir) > 0
+    assert isinstance(rir, np.ndarray)
+    assert np.any(np.abs(rir) >= 1e-6)
